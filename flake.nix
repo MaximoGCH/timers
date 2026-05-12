4ede7a9f66292435e356d7066107f49516a142a3
MaximoGCH <maximo.garcia.cesto@gmail.com>
2026-05-12 09:37:18 +0000

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Minimal Zig development shell";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [
+          pkgs.zig
+        ];
+      };
+    };
+}
